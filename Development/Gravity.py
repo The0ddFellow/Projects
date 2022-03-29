3e36d3c8c6f3a498c613757c19aa9ea986be4d8c
The Odd Fellow <stephanrodz@gmail.com>
2022-03-29 09:52:51 -0500

    Messing around with Vim

--- Development/Gravity.py
+++ Development/Gravity.py
@@ -2,15 +2,12 @@
 
 g_earth = 9.81
 g_moon = 1.62
-g_sun = 274
-m_person = 68
+g_sun = 274.00
+m_person = 68.00
 
-gravity = [g_earth, g_moon, g_sun]
-
-m_person = 68
+gravity = [g_moon, g_earth, g_sun] 
 
 force = [m_person * i for i in gravity]
 
-print(force)
-
+print(force) 
 
