f8c836cbac47ca9162586fcc9dd95663a6148c51
The Odd Fellow <stephanrodz@gmail.com>
2022-03-29 16:27:02 -0500

    Added stuff and more readability

--- Development/Gravity.py
+++ Development/Gravity.py
@@ -3,11 +3,14 @@
 g_earth = 9.81
 g_moon = 1.62
 g_sun = 274.00
-m_person = 68.00
+m_person = 149.68
 
 gravity = [g_moon, g_earth, g_sun] 
 
-force = [m_person * i for i in gravity]
+forceN = [(m_person * i) for i in gravity]
 
-print(force) 
+print("In Newtons \nMoon: " + "{:.2f}".format(forceN[0]) + ", Earth: " + "{:.2f}".format(forceN[1]) + ", Sun: " + "{:.2f}".format(forceN[2]) + "\n")
+
+forceLb = [i / 4.45 for i in forceN] 
+print("In Pounds \nMoon: " + "{:.2f}".format(forceLb[0]) + ", Earth: " + "{:.2f}".format(forceLb[1]) + ", Sun: " + "{:.2f}".format(forceLb[2])) 
 
